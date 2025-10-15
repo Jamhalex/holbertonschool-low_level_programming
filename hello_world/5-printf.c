8aca0896376af5b4fe558a3ed1f51a5ce17f6baf
jamhalex <alexdoce823@gmail.com>
2025-10-15 11:24:10 -0400

    task5

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 
 /**
- *  main - Prints a message using printf
- *  Description: This function prints
- *  return is always 0
+ * main - Prints a message using printf
+ *
+ * Description: This function prints the phrase
+ * "with proper grammar, but the outcome is a piece of art,"
+ * followed by a new line, using the printf function.
+ *
+ * Return: Always 0 (Success)
  */
 
 int main(void)
