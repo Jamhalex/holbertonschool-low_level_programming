767b93e533687c5cae3803a12d3ac9741eddcd63
jamhalex <alexdoce823@gmail.com>
2025-10-15 10:49:28 -0400

    task5

---
+++ hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/*
+ * C program that prints x followed  by new line
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
