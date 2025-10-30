48810a43939077bfabe389b9d2c0097d3887faff
jamhalex <alexdoce823@gmail.com>
2025-10-30 07:29:06 -0400

    pointers and arrays

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -13,49 +13,43 @@
  */
 int _atoi(char *s)
 {
-	int i;
-	int sign;
-	int started;
-	int res;
-
-	sign = 1;
-	res = 0;
-	started = 0;
-
-	for (i = 0; s[i] != '\0'; i++)
-	{
-			if (!started && (s[i] == '-' || s[i] == '+'))
-			{
-						if (s[i] == '-')
-							sign = -sign;
-						continue;
-			}
-
-			if (s[i] >= '0' && s[i] <= '9')
-			{
-						int d = s[i] - '0';
-
-						started = 1;
-
-						if (sign > 0)
-						{
-										if (res > (INT_MAX - d) / 10)
-											return (INT_MAX);
-										res = res * 10 + d;
-						}
-						else
-						{
-										/* Build as negative to avoid INT_MIN flip overflow */
-										if (res < (INT_MIN + d) / 10)
-											return (INT_MIN);
-										res = res * 10 - d;
-						}
-			}
-			else if (started)
-			{
-						break;
-			}
-	}
-
-	return (res);
+int i;
+int sign;
+int started;
+int res;
+sign = 1;
+res = 0;
+started = 0;
+for (i = 0; s[i] != '\0'; i++)
+{
+if (!started && (s[i] == '-' || s[i] == '+'))
+{
+if (s[i] == '-')
+sign = -sign;
+continue;
+}
+if (s[i] >= '0' && s[i] <= '9')
+{
+int d = s[i] - '0';
+started = 1;
+if (sign > 0)
+{
+if (res > (INT_MAX - d) / 10)
+return (INT_MAX);
+res = res * 10 + d;
+}
+else
+{
+/* Build as negative to avoid INT_MIN flip overflow */
+if (res < (INT_MIN + d) / 10)
+return (INT_MIN);
+res = res * 10 - d;
+}
+}
+else if (started)
+{
+break;
+}
+}
+return (res);
 }
