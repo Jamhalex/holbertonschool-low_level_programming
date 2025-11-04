1fcb176ac92399c00d8f42897d723457ea74819e
jamhalex <alexdoce823@gmail.com>
2025-11-04 19:36:56 -0400

    pointers and arrays

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strcat - Concatenates two strings.
+ * @dest: Destination buffer; must have enough space.
+ * @src:  Source string to append.
+ *
+ * Description:
+ * Appends the string pointed to by @src to the end of @dest.
+ * Overwrites the terminating null byte of @dest, then adds a new '\0'.
+ *
+ * Return: Pointer to @dest.
+ */
+
+char *_strcat(char *dest, char *src)
+{
+char *p = dest;
+
+	/* move p to the end of dest */
+	while (*p)
+		p++;
+
+	/* copy src (without its '\0') to the end of dest */
+	while (*src)
+		*p++ = *src++;
+
+	/* add the new string terminator */
+	*p = '\0';
+
+	return (dest);
+
+}
---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strncpy - Copy at most n bytes from src to dest.
+ * @dest: Destination buffer; must be large enough for n bytes.
+ * @src: Source string.
+ * @n: Maximum number of bytes to copy from src.
+ *
+ * Description:
+ * Copies up to n bytes from @src to @dest. If @src has fewer than n bytes,
+ * the remainder of @dest is padded with '\0'. If @src has n or more bytes,
+ * no terminating null byte is added to @dest (strncpy semantics).
+ *
+ * Return: Pointer to @dest.
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+	for (; i < n; i++)
+		dest[i] = '\0';
+
+	return (dest);
+}
