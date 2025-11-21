44effd8924843fc61d447ebf0a009b1ab131e8d1
Jamhalex <jamhalex@gmail.com>
2025-11-21 06:45:04 -0400

    strcat

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -1,31 +1,30 @@
 #include "main.h"
 #include <stdio.h>
+
 /**
  * _strncat - Concatenates two strings.
  * @dest: Destination buffer; must have enough space.
- * @src:  Source string to append.
- *
- * Description:
- * Appends the string pointed to by @src to the end of @dest.
- * Overwrites the terminating null byte of @dest, then adds a new '\0'.
- *
- * Return: Pointer to @dest.
+ * return - dest
  */
 
 char *_strncat(char *dest, char *src)
 {
-char *p = dest;
+	int j = 0;
+	int i = 0;
 
-	/* move p to the end of dest */
-	while (*p)
-		p++;
+	while (dest[j] != '\0')
+	{
+		j++;
+	}
 
-	/* copy src (without its '\0') to the end of dest */
-	while (*src)
-		*p++ = *src++;
-
-	/* add the new string terminator */
-	*p = '\0';
+	while (src[i] != '\0' && i < n)
+	{
+		dest[j] = src[i];
+		j++;
+		i++;
+	}
+	
+	dest[j] = '\0';
 
 	return (dest);
 
