d2a5b0abd326c57dd9bd0c4750d5fd18e0db4b16
Jamhalex <jamhalex@gmail.com>
2025-11-28 20:12:36 -0400

    task 1

--- variadic_functions/main.c
+++ variadic_functions/main.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include "variadic_functions.h"
 
 /**
@@ -6,13 +5,8 @@
  *
  * Return: Always 0.
  */
-
-
 int main(void)
 {
-  int sum;
-
-  sum = sum_them_all(2, 98, 1024);
-  printf("%d\n", sum);
-  return (0);
+    print_numbers(", ", 4, 0, 98, -1024, 402);
+    return (0);
 }
