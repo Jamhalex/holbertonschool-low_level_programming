955681ae5a9530956674c80abcca154be0ab7558
Johnson <jamhalex@gmail.com>
2025-12-09 07:15:34 -0400

    task 7: kernell style

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -3,49 +3,36 @@
 
 /**
  * insert_dnodeint_at_index - inserts a new node at a given index
- * @h: double pointer to the head of the list
- * @idx: index at which to insert the new node (starts at 0)
- * @n: integer value to store in the new node
+ * @h: double pointer to head
+ * @idx: index to insert at (starts at 0)
+ * @n: value to store in the new node
  *
- * Return: address of the new node, or NULL if it fails
+ * Return: address of the new node, or NULL on failure
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *new, *temp;
-	unsigned int i = 0;
+	unsigned int i;
 
 	if (h == NULL)
 		return (NULL);
-
-	/* insert at head */
 	if (idx == 0)
 		return (add_dnodeint(h, n));
 
 	temp = *h;
-
-	/* traverse to the node before insertion position */
-	while (temp != NULL && i < idx - 1)
-	{
+	for (i = 0; temp != NULL && i < idx - 1; i++)
 		temp = temp->next;
-		i++;
-	}
 
-	/* idx out of bounds (middle insertion impossible) */
 	if (temp == NULL)
 		return (NULL);
-
-	/* insert at end */
 	if (temp->next == NULL)
 		return (add_dnodeint_end(h, n));
 
-	/* allocate new node for middle insertion */
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
 		return (NULL);
 
 	new->n = n;
-
-	/* rewire pointers */
 	new->next = temp->next;
 	new->prev = temp;
 	temp->next->prev = new;
