caa16159209827a935a9e80880c73f91e06ef2f6
Johnson <jamhalex@gmail.com>
2025-12-14 07:49:20 -0400

    task ++

---
+++ hash_tables/3-hash_table_set.c
@@ -0,0 +1,66 @@
+#include "hash_tables.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * hash_table_set - adds or updates an element in the hash table
+ * @ht: pointer to the hash table
+ * @key: key to add/update (cannot be empty)
+ * @value: value associated with the key (must be duplicated)
+ *
+ * Return: 1 on success, 0 on failure
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int idx;
+	hash_node_t *node;
+	hash_node_t *new_node;
+	char *new_value;
+
+	if (ht == NULL || ht->array == NULL || key == NULL || *key == '\0'
+	    || value == NULL)
+		return (0);
+
+	idx = key_index((const unsigned char *)key, ht->size);
+
+	node = ht->array[idx];
+	while (node != NULL)
+	{
+		if (strcmp(node->key, key) == 0)
+		{
+			new_value = strdup(value);
+			if (new_value == NULL)
+				return (0);
+
+			free(node->value);
+			node->value = new_value;
+			return (1);
+		}
+		node = node->next;
+	}
+
+	new_node = malloc(sizeof(hash_node_t));
+	if (new_node == NULL)
+		return (0);
+
+	new_node->key = strdup(key);
+	if (new_node->key == NULL)
+	{
+		free(new_node);
+		return (0);
+	}
+
+	new_node->value = strdup(value);
+	if (new_node->value == NULL)
+	{
+		free(new_node->key);
+		free(new_node);
+		return (0);
+	}
+
+	new_node->next = ht->array[idx];
+	ht->array[idx] = new_node;
+
+	return (1);
+}
+
