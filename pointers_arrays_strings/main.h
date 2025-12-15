a3e8f55dd6e86d78acd64101fa6ab5a21b792e93
Johnson <jamhalex@gmail.com>
2025-12-14 20:37:23 -0400

    task 0:

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -22,5 +22,6 @@ void reverse_array(int *a, int n);
 char *string_toupper(char *);
 char *cap_string(char *);
 char *_strncat(char *dest, char *src, int n);
+char *_memset(char *s, char b, unsigned int n);
 
 #endif /* MAIN_H */
