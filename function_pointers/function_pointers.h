9b3c210ac816d0793d315772f9fee3b8877274a8
Johnson <jamhalex@gmail.com>
2025-12-16 12:42:41 -0400

    task 1

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -2,16 +2,10 @@
 #define FUNCTION_POINTERS_H
 
 void print_name(char *name, void (*f)(char *));
-
+void array_iterator(int *array, size_t size, void (*action)(int));
 int _putchar(char c);
 
 #endif /* FUNCTION_POINTERS_H */
-#ifndef FUNCTION_POINTERS_H
-#define FUNCTION_POINTERS_H
 
-void print_name(char *name, void (*f)(char *));
 
-int _putchar(char c);
-
-#endif /* FUNCTION_POINTERS_H */
 
