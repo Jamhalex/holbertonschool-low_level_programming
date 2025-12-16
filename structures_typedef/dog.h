ac100cd0cb752e206b0ba772f9b549c4dfe3820c
Johnson <jamhalex@gmail.com>
2025-12-15 22:56:43 -0400

    dog struct init

---
+++ structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - structure describing a dog
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+void init_dog(struct dog *d, char *name, float age, char *owner);
+#endif /* DOG_H */
+
